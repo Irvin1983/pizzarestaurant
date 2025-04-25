17ba53627e28f05b03017d0da46d237c2fab05bf
Irvin1983 <24006379@uhi.ac.uk>
2025-04-25 17:31:59 +0100

    Delete PizzaWebsite directory

--- PizzaWebsite/javascript/contact.js
+++
@@ -1,6 +0,0 @@
-// Creating an alert to appear when submit button is pressed.
-
-
-document.getElementById("my-form").onsubmit = function() {
-	window.alert("Thanks for contacting us, we will get back to you asap.")
-};
